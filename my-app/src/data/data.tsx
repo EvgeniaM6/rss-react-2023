66403f40d88d23beec382bc1d251aaa308d9f353
EvgeniaM6 <evgminukova6@gmail.com>
2023-03-18 20:28:09 +0200

    feat: complete the database

--- my-app/src/data/data.tsx
+++ my-app/src/data/data.tsx
@@ -2,7 +2,7 @@ export const dataBase = {
   goods: [
     {
       id: 1,
-      name: 'hoodie ukrainian in black color',
+      name: 'hoodie ukrainian',
       category: 'hoodie',
       brand: 'keepstyle',
       country: 'Ukraine',
@@ -16,31 +16,314 @@ export const dataBase = {
           code: '#000000',
         },
       ],
-      composition: 'cotton 80% elastane 20%',
+      composition: {
+        short: 'cotton 80%, elastane 20%',
+        full: 'cotton 80% elastane 20%',
+      },
       care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
       price: '2199',
       sizes: ['S', 'M', 'L'],
+      sex: 'male',
     },
     {
       id: 2,
-      name: 'світшот not refugee у кольорі vanilla',
-      category: 'hoodie',
+      name: 'sweatshirt not refugee',
+      category: 'sweatshirt',
       brand: 'keepstyle',
       country: 'Ukraine',
       imageUrl:
-        'https://keepstyle.co/storage/uploads/products/images/0efc357366cef5fd090f4e3c0d467393.jpg',
+        'https://keepstyle.co/storage/uploads/products/images/9511815c45a2795b3b68c20b0af6f839.jpg',
       description:
-        'hoodie with raglan sleeves made of lining style - free oversize, items from charity projects, where all profits are transferred to charitable funds, are not subject to return',
+        'oversize sweatshirt in collaboration with Vova Dantes to the release of the track Not A Refugee, with a print and with a drop shoulder, a print on the front and on the back',
+      colors: [
+        {
+          title: 'Vanilla',
+          code: '#fffce8',
+        },
+      ],
+      composition: {
+        short: '100% cotton',
+        full: '100% cotton, lining material, soft and pleasant to the body, it is not hot in summer, but warm in cold weather, thanks to its composition, it is quite wear-resistant',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '2449',
+      sizes: ['S', 'L'],
+      sex: 'male',
+    },
+    {
+      id: 3,
+      name: 'mriya t-shirt',
+      category: 't-shirt',
+      brand: 'keepstyle',
+      country: 'Ukraine',
+      imageUrl:
+        'https://keepstyle.co/storage/uploads/products/images/44f9a01d14f22745b7f0cadb5a401849.jpg',
+      description:
+        'oversize t-shirt with a print on the front, non-returnable items from charity projects where all profits are transferred to charitable funds',
+      colors: [
+        {
+          title: 'Vanilla',
+          code: '#fffce8',
+        },
+      ],
+      composition: {
+        short: 'cotton 95%, polyester 5%',
+        full: 'cotton 95%, polyester 5%, material cooler',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '1189',
+      sizes: ['XS', 'S', 'L'],
+      sex: 'male',
+    },
+    {
+      id: 4,
+      name: 't-shirt the future is ours',
+      category: 't-shirt',
+      brand: 'keepstyle',
+      country: 'Ukraine',
+      imageUrl:
+        'https://keepstyle.co/storage/uploads/products/images/656f2517975c531249546f7ad7fdb87d.jpg',
+      description:
+        'oversize off-the-shoulder design, front chest print, crewneck collar gen.camp collaboration t-shirt “future is ours”',
+      colors: [
+        {
+          title: 'blue',
+          code: '#b9d2e9',
+        },
+      ],
+      composition: {
+        short: 'cotton 95%, polyester 5%',
+        full: 'cotton 95%, polyester 5%, material cooler',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '979',
+      sizes: ['S', 'M', 'L'],
+      sex: 'male',
+    },
+    {
+      id: 5,
+      name: 'sweatshirt future',
+      category: 'sweatshirt',
+      brand: 'keepstyle',
+      country: 'Ukraine',
+      imageUrl:
+        'https://keepstyle.co/storage/uploads/products/images/ed4dfe3ced372d8888aea78cfb41b75a.jpg',
+      description:
+        'oversize sweatshirt with a print and off the shoulder sweatshirt from the collaboration with gen.camp “future is ours”',
+      colors: [
+        {
+          title: 'white',
+          code: '#f7f7f7',
+        },
+      ],
+      composition: {
+        short: 'cotton 80%, polyester 20%',
+        full: 'cotton 80%, polyester 20%, lining material, soft and pleasant to the body, it is not hot in summer, but warm in cold weather, due to its composition, it is quite durable',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '1369',
+      sizes: ['S', 'L'],
+      sex: 'male',
+    },
+    {
+      id: 6,
+      name: 'Pleated skirt',
+      category: 'skirt',
+      brand: 'fashionista',
+      country: 'Ukraine',
+      imageUrl: 'https://fashionista.ua/115297-large_default/spidnicya-z-plisiruvannyam.jpg',
+      description:
+        'Plain pleated mini length skirt. A laconic model with a high fit and smooth finished bottom. Fastens with a hidden zipper on the side',
+      colors: [
+        {
+          title: 'gray',
+          code: '#aab2bd',
+        },
+        {
+          title: 'milk',
+          code: '#fff5ec',
+        },
+        {
+          title: 'fuchsia',
+          code: '#ff3c8d',
+        },
+        {
+          title: 'pistachio',
+          code: '#c6d9ad',
+        },
+      ],
+      composition: {
+        short: 'viscose 32%, lycra 5%, polyester 63%',
+        full: 'viscose 32%, lycra 5%, polyester 63%',
+      },
+      care: 'Machine wash at a maximum temperature of 30 °C. Normal drum drying (spinning and drying are allowed). Iron at a temperature of 150 °C. Normal dry cleaning (using Perchloroethylene). Bleaching is prohibited',
+      price: '890',
+      sizes: ['XS', 'S', 'M', 'L'],
+      sex: 'female',
+    },
+    {
+      id: 7,
+      name: 'hoodie Support',
+      category: 'hoodie',
+      brand: 'Support by Poustovit',
+      country: 'Ukraine',
+      imageUrl:
+        'https://supportbypoustovit.com/wp-content/uploads/2023/01/photo_5377437344405242603_y.jpg',
+      description: 'Insulated hoodie with "FIGHTING RED / MEMORIZING BLACK" print',
+      colors: [
+        {
+          title: 'milk',
+          code: '#eeeeee',
+        },
+      ],
+      composition: {
+        short: '80% cotton, 20% polyester',
+        full: '80% cotton, 20% polyester',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '3100',
+      sizes: ['S', 'L', 'XXL'],
+      sex: 'female',
+    },
+    {
+      id: 8,
+      name: 'hoodie WITH FLUORESCENT LEFT RIGHT BANK & CHESTNUT',
+      category: 'hoodie',
+      brand: 'Support by Poustovit',
+      country: 'Ukraine',
+      imageUrl:
+        'https://supportbypoustovit.com/wp-content/uploads/2020/04/photo_5386596731531151774_y.jpg',
+      description:
+        'hoodie with fluorescent embroidery "LEFT COAST RIGHT COAST" and a chestnut leaf',
+      colors: [
+        {
+          title: 'pink',
+          code: '#d3a1ba',
+        },
+        {
+          title: 'orange',
+          code: '#fd5232',
+        },
+        {
+          title: 'black',
+          code: '#221e1f',
+        },
+      ],
+      composition: {
+        short: '100% cotton',
+        full: '100% cotton',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '2900',
+      sizes: ['S', 'L', 'XXL'],
+      sex: 'female',
+    },
+    {
+      id: 9,
+      name: 'OVERSIZE SWEATSHIRT "KYIV"',
+      category: 'sweatshirt',
+      brand: 'DiaDia',
+      country: 'Ukraine',
+      imageUrl:
+        'https://diadia.ua/image/cachewebp/catalog/2023-02/photo1502/D10201140_2-597x796.webp',
+      description:
+        'Oversized sweatshirt. There are cuffs at the bottom and on the sleeves. Dropped shoulder. The print on the front is made with quality DTF inks. The inscription is resistant to washing. Knitwear without insulation',
+      colors: [
+        {
+          title: 'milk',
+          code: '#f0f2ef',
+        },
+      ],
+      composition: {
+        short: '95% cotton, 5% elastane',
+        full: '95% cotton, 5% elastane',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '1390',
+      sizes: ['S', 'L'],
+      sex: 'female',
+    },
+    {
+      id: 10,
+      name: 'INSULATED SWEATSHIRT "UKRAINE"',
+      category: 'sweatshirt',
+      brand: 'DiaDia',
+      country: 'Ukraine',
+      imageUrl:
+        'https://diadia.ua/image/cachewebp/catalog/2022-10/photo1310/D10240024_1-750x1000.webp',
+      description:
+        'Oversized sweatshirt. There are cuffs at the bottom and on the sleeves. Dropped shoulder. The print on the front is made with quality DTF inks. The inscription is resistant to washing',
+      colors: [
+        {
+          title: 'blue',
+          code: '#0058b0',
+        },
+        {
+          title: 'milk',
+          code: '#f0f2ef',
+        },
+      ],
+      composition: {
+        short: '85% cotton, 15% elastane',
+        full: '85% cotton, 15% elastane',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '1290',
+      sizes: ['S', 'L'],
+      sex: 'female',
+    },
+    {
+      id: 11,
+      name: 'hoodie WILL',
+      category: 'hoodie',
+      brand: 'Support by Poustovit',
+      country: 'Ukraine',
+      imageUrl:
+        'https://supportbypoustovit.com/wp-content/uploads/2022/05/photo_5386596731531151775_y.jpg',
+      description: 'hoodie WILL with a print on the chest',
+      colors: [
+        {
+          title: 'black',
+          code: '#221e1f',
+        },
+        {
+          title: 'milk',
+          code: '#f0f2ef',
+        },
+      ],
+      composition: {
+        short: '100% cotton',
+        full: '100% cotton',
+      },
+      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
+      price: '3000',
+      sizes: ['S', 'L', 'XXL'],
+      sex: 'female',
+    },
+    {
+      id: 12,
+      name: 'Dress with polka dot print and open shoulders',
+      category: 'dress',
+      brand: 'fashionista',
+      country: 'Ukraine',
+      imageUrl:
+        'https://fashionista.ua/131442-large_default/suknya-z-printom-polka-dot-ta-vidkritimi-plechima.jpg',
+      description:
+        'Printed mini dress with long flared sleeves. Model with open shoulders, elastic insert along the neckline and cut-off flare skirt. Fastens with a hidden zipper on the side',
       colors: [
         {
           title: 'black',
           code: '#000000',
         },
       ],
-      composition: 'cotton 80% elastane 20%',
-      care: 'hand or delicate wash up to 30 degrees is recommended, read the inner label in detail before washing',
-      price: '2199',
-      sizes: ['S', 'M', 'L'],
+      composition: {
+        short: '100% viscose',
+        full: '100% viscose',
+      },
+      care: 'Machine wash at a maximum temperature of 30 °C. Drum drying is prohibited. Iron at a temperature of 110 °C. Normal dry cleaning (using Perchloroethylene). Bleaching is prohibited.',
+      price: '1690',
+      sizes: ['XS', 'S', 'M'],
+      sex: 'female',
     },
   ],
 };
