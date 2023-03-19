1047403877aad0e7ca6a20fd50a53e4e498fdbb6
EvgeniaM6 <evgminukova6@gmail.com>
2023-03-19 22:14:18 +0200

    feat: add testing

---
+++ my-app/src/test/App.test.tsx
@@ -0,0 +1,7 @@
+import App from '../components/App';
+
+describe('App component', () => {
+  it('should be created', () => {
+    expect(App).toBeDefined();
+  });
+});
---
+++ my-app/src/test/GoodsList.test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { GoodsList } from '../components/GoodsList';
+import { dataBase } from '../data/data';
+
+describe('GoodsList', () => {
+  beforeEach(() => {
+    render(<GoodsList />);
+  });
+
+  it('should render all good cards', () => {
+    const goodCardElementsList = screen.getAllByTestId('good-card');
+    const goodsAmount = dataBase.goods.length;
+    expect(goodCardElementsList).toHaveLength(goodsAmount);
+  });
+});
