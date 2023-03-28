91bfe6dd893894d94b839214684b91daaf79755a
EvgeniaM6 <evgminukova6@gmail.com>
2023-03-29 00:18:20 +0300

    feat: change search input and goods cards to functional component

--- my-app/src/components/goods/GoodsList.tsx
+++ my-app/src/components/goods/GoodsList.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { GoodCard } from './GoodCard';
 import { dataBase } from '../../data/data';
 
-export class GoodsList extends Component {
-  render() {
-    return (
-      <div className="goods">
-        {dataBase.goods.map((goodObj) => (
-          <GoodCard key={goodObj.id} good={goodObj} />
-        ))}
-      </div>
-    );
-  }
+export function GoodsList() {
+  return (
+    <div className="goods">
+      {dataBase.goods.map((goodObj) => (
+        <GoodCard key={goodObj.id} good={goodObj} />
+      ))}
+    </div>
+  );
 }
