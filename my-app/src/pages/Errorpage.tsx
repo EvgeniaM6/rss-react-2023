95c602e0c12d5178fdcac62eb67c30e9710dfd2a
EvgeniaM6 <evgminukova6@gmail.com>
2023-03-29 00:17:43 +0300

    feat: change about-page and error-page to functional component

--- my-app/src/pages/Errorpage.tsx
+++ my-app/src/pages/Errorpage.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { TPropsHandle } from '../models';
 import { PAGES } from '../constants/pages';
 
-export class Errorpage extends Component<TPropsHandle> {
-  componentDidMount(): void {
-    this.props.handleOpenPage(PAGES.error);
-  }
+export function Errorpage(props: TPropsHandle) {
+  useEffect(() => {
+    props.handleOpenPage(PAGES.error);
+  }, []);
 
-  render() {
-    return (
-      <div className="container error-page">
-        <p>Error 404</p>
-        <h1>Oops! Specified page not found</h1>
-      </div>
-    );
-  }
+  return (
+    <div className="container error-page">
+      <p>Error 404</p>
+      <h1>Oops! Specified page not found</h1>
+    </div>
+  );
 }
