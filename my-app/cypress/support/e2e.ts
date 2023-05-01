b53f4d904ea32b68f761ef1d2a87da3eb9540de9
EvgeniaM6 <evgminukova6@gmail.com>
2023-05-01 14:08:21 +0300

    fix: fix e2e tests error

--- my-app/cypress/support/e2e.ts
+++ my-app/cypress/support/e2e.ts
@@ -19,3 +19,6 @@ import './commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 import '@cypress/code-coverage/support';
+afterEach(() => {
+  cy.window().trigger('unload');
+});
